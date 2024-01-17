b631ce3434f3c20b040b4f3453b456c22f253c09
KYWILS21 <156925256+KYWILS21@users.noreply.github.com>
2024-01-17 14:00:15 -0500

    Update random.c

--- random.c
+++ random.c
@@ -2,7 +2,23 @@
 #include <stdlib.h>
 #include <time.h>
 
-char randchar(){
-  char random = 'A' + (random() % 26);
-return random;
+int randchar();
+
+int main(){
+	int a;
+
+	/* seed the randomizer */
+	srand( (unsigned)time(NULL) );
+
+	printf("Today's random word: ");
+	for(a=0;a<7;a++)
+		putchar( randchar() );
+	putchar('\n');
+
+	return(0);
+}
+
+int randchar(){
+  int randomChar = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"[random () % 26];
+return (long int)randomChar;
 }
